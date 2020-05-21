0a98c9cbeb84b9bfb24954b2ff5a5f864e6e4236
Amit Dhakad <amit0502dhakad@gmail.com>
2020-05-21 21:19:06 +0530

    update menu item to navigate withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import MenuItem from '../menu-item/menu-tem.component'
+import MenuItem from '../menu-item/menu-item.component'
 import './directory.styles.scss'
 export default class Directory extends Component {
 
@@ -12,7 +12,7 @@ export default class Directory extends Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
                   },
                   {
                     title: 'jackets',
@@ -47,8 +47,8 @@ export default class Directory extends Component {
         return (
             <div className="directory-menu">
             {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}></MenuItem>
+                this.state.sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps}></MenuItem>
                 ))
             }
             </div>
