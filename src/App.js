57929414bcd2a92d1726816160adb0912630755e
Amit Dhakad <amit0502dhakad@gmail.com>
2020-05-22 22:33:45 +0530

    added sign in component and added inut fields

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = () => (
   <div>
@@ -16,7 +17,8 @@ function App() {
       <Header />
        <Switch>
       <Route exact  path='/' component= {HomePage} />
-      <Route   path='/shop' component= {ShopPage} />
+      <Route exact  path='/shop' component= {ShopPage} />
+      <Route excat path='/signin' component={SignInAndSignUpPage} />
        </Switch>
     </div>
   );
