9900022bbb6617117e617b9cb94219798a7ebca3
Amit Dhakad <amit0502dhakad@gmail.com>
2020-05-25 21:56:42 +0530

    storing user data in database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.util.js';
+import {auth, createUserProfileDocument} from './firebase/firebase.util.js';
 
 const HatsPage = () => (
   <div>
@@ -25,9 +25,8 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null
   componentDidMount() {
-   this.unsubscribeFromAuth =  auth.onAuthStateChanged(user => {
-     this.setState({currentUser:user})
-     console.log('user: ', user);
+   this.unsubscribeFromAuth =  auth.onAuthStateChanged(async user => {
+       createUserProfileDocument(user);
    })
   }
 
